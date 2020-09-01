3b4d382bb17b364fface198674dc71471157b605
mertcanfsm <69803271+mertcanfsm@users.noreply.github.com>
2020-09-01 15:10:10 +0300

    Service update

--- app/src/main/java/com/example/myapplication/network/Services.java
+++ app/src/main/java/com/example/myapplication/network/Services.java
@@ -14,9 +14,8 @@ import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface Services {
-    // @GET("BisikletDuraklariJSON")
-    //Call<List<BikeStation>> getBikeStation();
-    Call<List<Randevu>> getRandevu();
+    
+    // Randevu
     @GET("randevu")
     Call<List<Randevu>> getRandevular();
     
@@ -28,12 +27,62 @@ public interface Services {
     
     @POST("randevu")
     Call<Randevu> createRandevu(@Body Randevu randevu);
-
-    //User
+    
+    @DELETE("randevu/{id}")
+    Call<ResponseBody> deleteRandevu(@Path("id") int randevuId);
+    
+    @PUT("randevu/{id}")
+    Call<ResponseBody> updateRandevu(@Path("id") int randevuId, @Body Randevu randevu);
+    
+    
+    // Ogrenci
+    @GET("ogrenci")
+    Call<List<OgrenciModel>> getOgrenciler();
+    
+    @GET("ogrenci/{id}")
+    Call<OgrenciModel> getOgrenci(@Path("id") int ogrenciId);
+    
     @POST("ogrenci")
-    Call<OgrenciModel> creatOgrenci(@Body Randevu randevu);
-
+    Call<OgrenciModel> createOgrenci(@Body OgrenciModel ogrenci);
+    
+    @DELETE("ogrenci/{id}")
+    Call<ResponseBody> deleteOgrenci(@Path("id") int ogrenciId);
+    
+    @PUT("ogrenci/{id}")
+    Call<ResponseBody> updateOgrenci(@Path("id") int ogrenciId, @Body OgrenciModel ogrenci);
+    
+    
+    // Donem
     @GET("donem")
-    Call<List<DonemModel>> getDonem();
+    Call<List<DonemModel>> getDonemler();
+    
+    @GET("donem/{id}")
+    Call<DonemModel> getDonem(@Path("id") int donemId);
+    
+    @POST("donem")
+    Call<DonemModel> createDonem(@Body DonemModel donem);
+    
+    @DELETE("donem/{id}")
+    Call<ResponseBody> deleteDonem(@Path("id") int donemId);
+    
+    @PUT("donem/{id}")
+    Call<ResponseBody> updateDonem(@Path("id") int donemId, @Body DonemModel donem);
+    
+    
+    // Yonetici
+    @GET("yonetici")
+    Call<List<Yonetici>> getYoneticiler();
+    
+    @GET("yonetici/{id}")
+    Call<Yonetici> getYonetici(@Path("id") int yoneticiId);
+    
+    @POST("yonetici")
+    Call<Yonetici> createYonetici(@Body Yonetici yonetici);
+    
+    @DELETE("yonetici/{id}")
+    Call<ResponseBody> deleteYonetici(@Path("id") int yoneticiId);
+    
+    @PUT("yonetici/{id}")
+    Call<ResponseBody> updateYonetici(@Path("id") int yoneticiId, @Body Yonetici yonetici);
 
 }
