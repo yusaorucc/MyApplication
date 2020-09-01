bbca82579fd57c91a3a024575420d30e5f24a058
mertcanfsm <69803271+mertcanfsm@users.noreply.github.com>
2020-09-01 15:13:17 +0300

    Update RegisterRequest

--- app/src/main/java/com/example/myapplication/model/RegisterRequest.java
+++ app/src/main/java/com/example/myapplication/model/RegisterRequest.java
@@ -1,4 +1,41 @@
 package com.example.myapplication.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class RegisterRequest {
+
+    @SerializedName("Username")
+    @Expose
+    public String Username;
+    @SerializedName("Email")
+    @Expose
+    public String Email;
+    @SerializedName("Password")
+    @Expose
+    public String Password;
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+  
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+  
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setBolum(String password) {
+        Password = password;
+    }
 }
